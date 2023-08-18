d8cc298bfaf67305c9c8f7c33bf657514fb8860e
Samuel Kociscak <s.kociscak@gmail.com>
2023-08-18 14:38:01 +0200

    more finely divided months

--- fetch_tswf_data.bat
+++ fetch_tswf_data.bat
@@ -8,9 +8,13 @@ call activate nanodust
 	start python solo_download_wrap.py "20200615" "20201231"
 	start python solo_download_wrap.py "20210101" "20210630"
 	start python solo_download_wrap.py "20210701" "20211231"
-	start python solo_download_wrap.py "20220101" "20220630"
-	start python solo_download_wrap.py "20220701" "20201231"
-	start python solo_download_wrap.py "20230101" "20230630"
+	start python solo_download_wrap.py "20220101" "20220331"
+	start python solo_download_wrap.py "20220401" "20220630"
+	start python solo_download_wrap.py "20220701" "20220930"
+	start python solo_download_wrap.py "20221001" "20221231"
+	start python solo_download_wrap.py "20230101" "20230331"
+	start python solo_download_wrap.py "20230401" "20230630"
+	start python solo_download_wrap.py "20230701" "20230930"
 	
 ) | set /P "="
 
