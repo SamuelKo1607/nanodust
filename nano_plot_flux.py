c516e43f0f2118e6616eb2b7f465d0d11a5a58fd
Samuel Kociscak <s.kociscak@gmail.com>
2023-09-29 11:07:39 +0200

    renaming for less confusion + adding some docs

--- nano_plot_flux.py
+++ nano_plot_flux.py
@@ -4,10 +4,11 @@ import matplotlib as mpl
 import glob
 import numpy as np
 import datetime as dt
+from scipy import stats
 
-from solo_load_days import load_list
-from solo_load_days import Impact
-from solo_load_days import Day
+from nano_load_days import load_list
+from nano_load_days import Impact
+from nano_load_days import Day
 from conversions import jd2date
 import figure_standards as figstd
 
@@ -20,6 +21,19 @@ peri_jd = 2459000.5+np.array([255,470,666,865,1045,1224])
 
 
 def load_all_days(days_location = "998_generated\\days\\"):
+    """
+    The function to load all the measurement days data.
+
+    Parameters
+    ----------
+    days_location : str, optional
+        The data directory. Default is "998_generated\\days\\".
+
+    Returns
+    -------
+    days : list of Day object
+        Measurement days, class Day from nano_load_days.
+    """
     files = glob.glob(days_location+"*.pkl")
     days = []
     for file in files:
@@ -29,27 +43,109 @@ def load_all_days(days_location = "998_generated\\days\\"):
     return days
 
 
-def plot_flux(days):
+def extract_variables_from_days(days):
+    """
+    The function to extract the relevant attributes from the provided days.
+
+    Parameters
+    ----------
+    days : list of Day object
+        Measurement days, class Day from nano_load_days.
+
+    Returns
+    -------
+    dates : np.array of float
+        The measuremetn days.
+    counts : np.array of int
+        The dust counts, enocutered on the dates.
+    duty_hours : np.array of float
+        The duty cycle in hours, per day. Often around 1.5.
+    sampling_rates : np.array of float
+        The sampling rate on that day. No dates with a vraiable 
+        sampling rate were processed in the first place.
+    """
     dates = np.zeros(0,dtype=dt.datetime)
-    counts = np.zeros(0)
-    duty_hours = np.zeros(0)
+    counts = np.zeros(0,dtype=int)
+    duty_hours = np.zeros(0,dtype=float)
+    sampling_rates = np.zeros(0,dtype=float)
     for day in days:
         dates = np.append(dates,day.date)
         counts = np.append(counts,day.impact_count)
         duty_hours = np.append(duty_hours, day.duty_hours)
+        sampling_rates = np.append(sampling_rates, day.sampling_rate)
+
+    return dates, counts, duty_hours, sampling_rates
+
+
+def get_errors(days, prob_coverage = 0.9):
+    """
+    The function to calculate the errorbars for flux 
+    assuming Poisson distribution and taking into account
+    the number of detection.
+
+    Parameters
+    ----------
+    days : list of Day object
+        Measurement days, class Day from nano_load_days.
+    prob_coverage : float, optional
+        The coverage of the errobar interval. The default is 0.9.
+
+    Returns
+    -------
+    err_plusminus_flux : np.array of float
+        The errorbars, lower and upper bound, shape (2, n).
+
+    """
+    dates, counts, duty_hours, sampling_rates = extract_variables_from_days(days)
+
+    counts_err_minus = -stats.poisson.ppf(0.5-prob_coverage/2, mu=counts)+counts
+    counts_err_plus  = +stats.poisson.ppf(0.5+prob_coverage/2, mu=counts)-counts
+    err_plusminus_flux = np.array([counts_err_minus,counts_err_plus]) / (duty_hours/(24))
+
+    return err_plusminus_flux
+
+
+def plot_flux(days, figures_location = "998_generated\\figures\\"):
+    """
+    A plot of daily flux is made with the data from the provided days files.
+
+    Parameters
+    ----------
+    days : list of Day object
+        Measurement days, class Day from nano_load_days.
+
+    figures_location : str, optional
+        Where to put the wrawn figure. Default is "figures_location".
+
+    Returns
+    -------
+    None.
+    """
+    dates, counts, duty_hours, sampling_rates = extract_variables_from_days(days)
+
+    err_plusminus_flux = get_errors(days)
+
+    colorcodes = np.zeros(0,dtype=str)
+    for sampling_rate in sampling_rates:
+        if sampling_rate < 263000:
+            colorcodes = np.append(colorcodes,"firebrick")
+        else:
+            colorcodes = np.append(colorcodes,"teal")
 
     fig, ax = plt.subplots(figsize=(3.8, 3))
     ax.set_ylabel("Impact rate (duty-cycle corrected) [$day^{-1}$]"
                   , fontsize="medium")
-    ax.set_title('CNN Dust Impacts: '+str(np.round(sum(counts)))[:-2]
+    ax.set_title('CNN Dust Impacts: '+str(np.round(sum(counts)))
                  , fontsize="medium", fontweight="bold")
     ax.tick_params(axis='x',labeltop=False,labelbottom=True)
     ax.tick_params(axis='y',labelleft=True,labelright=False)
     ax.tick_params(axis='y',which="minor",left=True,right=False)
     ax.scatter(dates,counts/duty_hours*24,
-               color="maroon", s=1,zorder=100)
-    #ax.errorbar(flux_date, flux_cnn, err_plusminus_flux_cnn,
-    #            color="firebrick", lw=0, elinewidth=0.4,alpha=0.35,zorder=100)
+               c=colorcodes, s=1,zorder=100)
+    for color in np.unique(colorcodes):
+        mask = colorcodes==color
+        ax.errorbar(dates[mask], counts[mask]/duty_hours[mask]*24, err_plusminus_flux[:,mask],
+                    c=color, lw=0, elinewidth=0.4,alpha=0.35,zorder=100)
     ax.xaxis.set_major_locator(mdates.MonthLocator(bymonth=(1, 3, 5, 7, 9, 11)))
     ax.xaxis.set_minor_locator(mdates.MonthLocator())
     ax.set_ylim(bottom = 0, top = 1000)
@@ -58,6 +154,10 @@ def plot_flux(days):
     ax.tick_params(axis='x',labelrotation=60)
     ax.tick_params(labelsize="medium")
     ax.set_ylim(0,1000)
+    ax.tick_params(labelleft=True,
+                   labelright=True,
+                   labelbottom = True,
+                   labeltop = False)
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
     for jd in apo_jd:
         ax.axvline(x=jd2date(jd),color="darkgray",lw=0.7)
@@ -66,8 +166,13 @@ def plot_flux(days):
     ax.text(.85, .96, 'Aphelion', ha='left', va='top', rotation=90, color="gray", fontsize="small", transform=ax.transAxes)
     ax.text(.74, .96, 'Perihelion', ha='left', va='top', rotation=90, color="gray", fontsize="small", transform=ax.transAxes)
     fig.tight_layout()
-    fig.savefig('cnn_flux.png', format='png', dpi=600)
+    fig.savefig(figures_location+'cnn_flux.png', format='png', dpi=600)
     fig.show()
+
+
+
+
+
 
 
 
@@ -75,37 +180,5 @@ plot_flux(load_all_days())
 
 
 
-"""
-
-fig, ax = plt.subplots(figsize=(3.8, 3))
-ax.set_ylabel("Impact rate (duty-cycle corrected) [$day^{-1}$]", fontsize=mylabelsize)
-ax.set_title('CNN Dust Impacts: '+str(np.round(sum(flux_cnn_imported)))[:-2], fontsize=mytitlesize, fontweight="bold")
-ax.tick_params(axis='x',labeltop=False,labelbottom=True)
-ax.tick_params(axis='y',labelleft=True,labelright=False)
-ax.tick_params(axis='y',which="minor",left=True,right=False)
-ax.scatter(flux_date, flux_cnn, color="maroon", s=1,zorder=100)
-ax.errorbar(flux_date, flux_cnn, err_plusminus_flux_cnn, color="firebrick", lw=0, elinewidth=0.4,alpha=0.35,zorder=100)
-ax.xaxis.set_major_locator(mdates.MonthLocator(bymonth=(1, 3, 5, 7, 9, 11)))
-ax.xaxis.set_minor_locator(mdates.MonthLocator())
-ax.set_ylim(bottom = 0, top = 1000)
-ax.set_xlim(left = min(flux_date), right = max(flux_date))
-ax.tick_params(axis='x',which="minor",bottom=True,top=True)
-ax.tick_params(axis='x',labelrotation=60)
-ax.tick_params(labelsize="medium")
-ax.set_ylim(0,1000)
-ax2 = ax.twinx()
-ax2.set_ylim(0,1000*0.0625)
-ax2.set_ylabel('Count [1]')
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
-for jd in apo_jd:
-    ax.axvline(x=jd2date(jd),color="darkgray",lw=0.7)
-for jd in peri_jd:
-    ax.axvline(x=jd2date(jd),color="darkgray",ls="--",lw=0.7)
-ax.text(.85, .96, 'Aphelion', ha='left', va='top', rotation=90, color="gray", fontsize="small", transform=ax.transAxes)
-ax.text(.74, .96, 'Perihelion', ha='left', va='top', rotation=90, color="gray", fontsize="small", transform=ax.transAxes)
-fig.tight_layout()
-fig.savefig('C:\\Users\\skoci\\Disk Google\\000 škola\\UIT\\presentations\\Venus 2023\\cnn_flux.png', format='png', dpi=600)
-fig.show()
 
 
-"""
