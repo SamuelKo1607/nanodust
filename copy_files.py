ad501ed26a543c49e8e29893f8013259eaf55846
Samuel Kociscak <s.kociscak@gmail.com>
2023-10-05 16:50:30 +0200

    initial

---
+++ copy_files.py
@@ -0,0 +1,24 @@
+import shutil
+import cdflib
+import glob
+import numpy as np
+from keys import cdf_tswf_e_location
+from nano_load_days import get_cdfs_to_analyze
+
+
+def main(destination, sampling_rate = 524275):
+    files = get_cdfs_to_analyze(cdf_tswf_e_location)
+    for file in files:
+        cdf_file = cdflib.CDF(file)
+        try:
+            file_sampling_rate = cdf_file.varget("sampling_rate")[0]
+            if np.isclose(file_sampling_rate, sampling_rate):
+                if len(glob.glob(destination+"*"+file[-16:]))>0:
+                    pass
+                else:
+                    shutil.copy(file, destination)
+        except:
+            pass
+
+
+#main("D:\\dust_cdfs_high_sampling_rate\\")
