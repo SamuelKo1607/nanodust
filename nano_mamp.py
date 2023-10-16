deb02d3093b50f55ee0944430eff167f49b1b190
Samuel Kociscak <s.kociscak@gmail.com>
2023-10-16 15:49:04 +0200

    moved the impactsuspect and loadallsuspects back to nano_mamp

--- nano_mamp.py
+++ nano_mamp.py
@@ -9,13 +9,92 @@ from keys import cdf_mamp_location
 
 from nano_load_days import get_cdfs_to_analyze
 from nano_load_days import load_all_days
-from nano_load_days import load_all_suspects
 from nano_load_days import load_list
 from nano_load_days import save_list
 from nano_load_days import Impact
 from nano_load_days import Day
-from nano_load_days import ImpactSuspect
 from conversions import tt2000_to_date
+
+
+
+class ImpactSuspect:
+    """
+    Each instance holds the attributes of 
+    one suspected dust impact as per MAMP. 
+    """
+    def __init__(self,
+                 datetime,
+                 sampling_rate,
+                 period,
+                 index,
+                 classification,
+                 channel_ref,
+                 amplitudes):
+        self.datetime = datetime
+        self.YYYYMMDD = datetime.strftime('%Y%m%d')
+        self.sampling_rate = sampling_rate
+        self.period = period
+        self.index = index
+        self.classification = classification
+        if classification == 1:
+            self.classification_readable = "Confirmed dust"
+        elif classification == -1:
+            self.classification_readable = "Confirmed non-dust"
+        else:
+            self.classification_readable = "Not confirmed"
+        self.channel_ref = channel_ref
+        self.amplitudes = amplitudes
+        self.produced = dt.datetime.now()
+
+    def info(self):
+        print(self.datetime.strftime("%d.%m.%Y %H:%M:%S, ")+
+              self.classification_readable + ","
+              " \n sampling rate: "+ str(self.sampling_rate)+
+              " \n amplitude: " + str(self.amplitudes))
+
+    def show(self):
+        print(vars(self))
+
+
+
+
+
+
+
+
+def load_all_suspects(location = "998_generated\\mamp_processed\\",
+                      date_from = dt.datetime(2010,1,1),
+                      date_to = dt.datetime(2050,1,1)):
+    """
+    The function to load all the suspected impacts as per MAMP.
+
+    Parameters
+    ----------
+    location : str, optional
+        The data directory. Default is "998_generated\\mamp_processed\\".
+    date_from : dt.datetime
+        The first relevant moment (filtering the instances by >=).
+    date_to : dt.datetime
+        The last relevant moment (filtering the instances by <=).
+
+    Returns
+    -------
+    suspects : list of ImpactSuspect object
+        Impact suspects, class ImpactSuspect.
+    """
+
+    files = glob.glob(location+"*.pkl")
+    suspects = []
+    for file in files:
+        suspect = load_list(file,"")
+        suspects.append(suspect)
+
+    flat_list_of_impacts = [item for sublist in suspects for item in sublist]
+
+    filtered = [i for i in flat_list_of_impacts if date_from <= i.datetime <= date_to ]
+
+    return filtered
+
 
 
 def is_confirmed(dust_datetimes,
