6defa71e8220ed80975bdbf64c0aa60be3f75b06
Samuel Kociscak <s.kociscak@gmail.com>
2023-10-19 10:33:50 +0200

    added os path patch

--- paths.py
+++ paths.py
@@ -1,8 +1,10 @@
-cdf_stat_location = "..\\data\\solo\\rpw\\tds_stat\\"
-cdf_tswf_e_location = "C..\\data\\solo\\rpw\\tds_wf_e\\"
-cdf_mamp_location = "..\\data\\solo\\rpw\\mamp\\"
-lo_f_cat_location = "..\\data\\solo\\rpw\\Amplitude data\\Index-Label-MaxAmplitude\\"
-lo_f_cat_new_location = "..\\data\\solo\\rpw\\Amplitude data\\low_frequency_new\\Label-MaxAmplitude\\"
-hi_f_cat_location = "..\\data\\solo\\rpw\\Amplitude data\\nanodust_classification\\all_events\\"
-solo_ephemeris_file = "..\\data\\ephemerides\\solo_fine_ephemeris_noheader.txt"
-venus_ephemeris_file = "..\\data\\ephemerides\\venus_fine_ephemeris_noheader.txt"
+import os
+
+cdf_stat_location = os.path.join(os.path.normpath(      "..\\data\\solo\\rpw\\tds_stat\\"), '')
+cdf_tswf_e_location = os.path.join(os.path.normpath(    "..\\data\\solo\\rpw\\tds_wf_e\\"), '')
+cdf_mamp_location = os.path.join(os.path.normpath(      "..\\data\\solo\\rpw\\mamp\\"), '')
+lo_f_cat_location = os.path.join(os.path.normpath(      "..\\data\\solo\\rpw\\Amplitude data\\Index-Label-MaxAmplitude\\"), '')
+lo_f_cat_new_location = os.path.join(os.path.normpath(  "..\\data\\solo\\rpw\\Amplitude data\\low_frequency_new\\Label-MaxAmplitude\\"), '')
+hi_f_cat_location = os.path.join(os.path.normpath(      "..\\data\\solo\\rpw\\Amplitude data\\nanodust_classification\\all_events\\"), '')
+solo_ephemeris_file = os.path.join(os.path.normpath(    "..\\data\\ephemerides\\solo_fine_ephemeris_noheader.txt"), '')
+venus_ephemeris_file = os.path.join(os.path.normpath(   "..\\data\\ephemerides\\venus_fine_ephemeris_noheader.txt"), '')
